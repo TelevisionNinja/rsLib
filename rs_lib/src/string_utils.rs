6bd6fa4518ce913d0d175b44c9014a04c4ab56d1
TelevisionNinja <TelevisionNinja@gmail.com>
2025-02-03 22:24:11 -0500

    detect more parameters

--- rs_lib/src/string_utils.rs
+++ rs_lib/src/string_utils.rs
@@ -15,7 +15,19 @@ pub fn rfind(string: &str, substring: &str, index: usize) -> Option<usize> {
 
 pub mod youtube {
     pub fn is_youtube_url(url: &str) -> bool {
-        url.starts_with("https://www.youtube.com/") || url.starts_with("https://youtube.com/") || url.starts_with("https://youtu.be/")
+        let starting_urls = [
+            "https://www.youtube.com/",
+            "https://youtube.com/",
+            "https://youtu.be/"
+        ];
+
+        for starting_url in starting_urls {
+            if url.starts_with(starting_url) {
+                return true;
+            }
+        }
+
+        false
     }
 
     pub fn short_to_video(url: &str) -> String {
@@ -31,21 +43,29 @@ pub mod youtube {
         url.to_string()
     }
 
-    pub fn remove_share_parameter(url: &str) -> String {
+    pub fn remove_tracking_parameters(url: &str) -> String {
         if is_youtube_url(url) {
-            let parameter = "si=";
-            let start_index_result = url.find(parameter);
+            let mut result = url.to_string();
 
-            if start_index_result.is_some() {
-                let start_index = start_index_result.unwrap();
-                // the '&' infront of 'si=' is assumed. thus we get rid of the '&' at the end
-                // another case: the '?' is infront of 'si='. thus we get rid of the '&' at the end
-                let end_index = super::find(url,"&", start_index).map_or(url.len(), |i| i + 1);
+            let parameters = [
+                "si",
+                "pp"
+            ];
 
-                let mut result = url.to_string();
-                result.replace_range(start_index..end_index, "");
-                return result;
+            for parameter in parameters {
+                let start_index_result = result.find(&(parameter.to_owned() + "="));
+
+                if start_index_result.is_some() {
+                    let start_index = start_index_result.unwrap();
+                    // the '&' infront of 'si=' is assumed. thus we get rid of the '&' at the end
+                    // another case: the '?' is infront of 'si='. thus we get rid of the '&' at the end
+                    let end_index = super::find(&result, "&", start_index).map_or(result.len(), |i| i + 1);
+
+                    result.replace_range(start_index..end_index, "");
+                }
             }
+
+            return result;
         }
 
         url.to_string()
@@ -85,9 +105,15 @@ mod tests {
         assert_eq!("https://www.youtube.com/watch?v=H3O6-SHr2fc", youtube::short_to_video("https://www.youtube.com/shorts/H3O6-SHr2fc"));
         assert_eq!("https://www.youtube.com/watch?v=HCE_lFUMXNg", youtube::short_to_video("https://www.youtube.com/watch?v=HCE_lFUMXNg"));
 
-        assert_eq!("https://youtu.be/2BO83Ig-E8E", youtube::remove_share_parameter("https://youtu.be/2BO83Ig-E8E"));
-        assert_eq!("https://youtube.com/shorts/60gZOXu5gcQ?", youtube::remove_share_parameter("https://youtube.com/shorts/60gZOXu5gcQ?si=EkAu2o2eUgp4SZV-"));
-        assert_eq!("https://www.youtube.com/shorts/H3O6-SHr2fc", youtube::remove_share_parameter("https://www.youtube.com/shorts/H3O6-SHr2fc"));
-        assert_eq!("https://www.youtube.com/watch?v=HCE_lFUMXNg", youtube::remove_share_parameter("https://www.youtube.com/watch?si=EkAu2o2eUgp4SZV-&v=HCE_lFUMXNg"));
+        assert_eq!("https://youtu.be/2BO83Ig-E8E", youtube::remove_tracking_parameters("https://youtu.be/2BO83Ig-E8E"));
+        assert_eq!("https://youtube.com/shorts/60gZOXu5gcQ?", youtube::remove_tracking_parameters("https://youtube.com/shorts/60gZOXu5gcQ?si=EkAu2o2eUgp4SZV-"));
+        assert_eq!("https://www.youtube.com/shorts/H3O6-SHr2fc", youtube::remove_tracking_parameters("https://www.youtube.com/shorts/H3O6-SHr2fc"));
+        assert_eq!("https://www.youtube.com/watch?v=HCE_lFUMXNg", youtube::remove_tracking_parameters("https://www.youtube.com/watch?si=EkAu2o2eUgp4SZV-&v=HCE_lFUMXNg"));
+
+        assert_eq!("https://youtube.com/shorts/60gZOXu5gcQ?", youtube::remove_tracking_parameters("https://youtube.com/shorts/60gZOXu5gcQ?pp=EkAu2o2eUgp4SZV-"));
+        assert_eq!("https://www.youtube.com/watch?v=HCE_lFUMXNg", youtube::remove_tracking_parameters("https://www.youtube.com/watch?pp=EkAu2o2eUgp4SZV-&v=HCE_lFUMXNg"));
+
+        assert_eq!("https://www.youtube.com/watch?v=HCE_lFUMXNg&", youtube::remove_tracking_parameters("https://www.youtube.com/watch?si=EkAu2o2eUgp4SZV-&v=HCE_lFUMXNg&pp=EkAu2o2eUgp4SZV-"));
+        assert_eq!("https://www.youtube.com/watch?v=HCE_lFUMXNg&", youtube::remove_tracking_parameters("https://www.youtube.com/watch?v=HCE_lFUMXNg&pp=EkAu2o2eUgp4SZV-&si=EkAu2o2eUgp4SZV-"));
     }
 }
